81f0ff36e8c8b64be62377c78dd39010239f6e73
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2024-11-26 16:16:51 +0530

    creted db config

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 require("dotenv").config();
+const { connectDb } = require("./config/db");
 
 // Initialization
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+connectDb();
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
